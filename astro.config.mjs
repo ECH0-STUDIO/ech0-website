1487df93f25f25ab19e0c7e01917dc251c66edc6
gigaloo <hello@ech0.work>
2026-05-11 22:19:16 +0700

    chore: swap Astro Node adapter for Cloudflare adapter for Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import cloudflare from '@astrojs/cloudflare';
 import react from '@astrojs/react';
-
-import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: node({
-    mode: 'standalone',
-  }),
+  adapter: cloudflare(),
   integrations: [react()]
 });
